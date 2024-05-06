d6d29d0081706682ba8a4c49d517835ef384f8e2
kelte13 <dim.tcka4ew@gmail.com>
2024-05-07 00:32:29 +0300

    не получившийся тест

--- max6675/src/receiving_data_max6675.c
+++ max6675/src/receiving_data_max6675.c
@@ -4,7 +4,7 @@ bool Max6675_Read_Reg(uint16_t *reg) {
     uint8_t temp[2] = {127, 248};
     //нужно перевести чипсет на 0, затем считать 2 байти данных, затем перевести чипселект на 1
     //получаем ответ от функции
-    bool hal_answer = HAL_SPI_Received();
+    bool hal_answer = HAL_SPI_Received(true);
     if (hal_answer == 1) {
         //проверка работается ли модуль
         if (temp[1] & 0x04) {
