f2fb8164f121bda4498b84b6948e4e38d8265445
kelte13 <dim.tcka4ew@gmail.com>
2024-05-06 21:33:09 +0300

    добавил функциию считывания данных

---
+++ max6675/test/test_receiving_data_max6675.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "receiving_data_max6675.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_receiving_data_max6675_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement receiving_data_max6675");
+}
+
+#endif // TEST
