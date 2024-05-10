8dcd55327453b551685c5f8bf23cb6427c0f867b
kelte13 <dim.tcka4ew@gmail.com>
2024-05-10 18:20:18 +0300

    все работает, но есть зависимость в пк от пути

--- max6675/src/main.c
+++ max6675/src/main.c
@@ -2,17 +2,19 @@
 #include "Max6675_Temperature.h"
 #include "receiving_data_max6675.h"
 #include "write_to_file.h"
+#include "delete_data_from_file.h"
 
 int main()
 {
     uint16_t reg = 0;
     float temperature = 0;
+    delete_data_from_file();
     for (size_t i = 0; i < 10; ++i) {
         bool answer = Max6675_Read_Reg(&reg, true);
         if (answer == MAX6675_OK) {
             //получение данных о температуре
             temperature = Max6675_Temperature(reg);
-            write_to_file( temperature);
+            write_to_file(temperature);
         }
         printf("temp = %f\n\r", temperature);
     }
--- max6675/src/write_to_file.c
+++ max6675/src/write_to_file.c
@@ -1,23 +1,13 @@
 #include "write_to_file.h"
 
 bool write_to_file(float temperature) {
-    //создаем файл
-    const char *filename = "temperature.txt";
-    // Дескриптор файла
-    int fd;
-    // Открытие файла в режиме добавления данных
-    fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
-    if (fd == -1) {
+    const char *filename = "C:\\\\Users\\\\DMITRIY\\\\Documents\\\\GitHub\\\\Pyroeffect\\\\max6675\\\\file\\\\temperature.txt";
+    FILE *file = fopen(filename, "a"); // Открыть файл для добавления данных
+    if (file == NULL) {
         perror("Ошибка при открытии файла");
         return MAX6675_ERROR;
     }
-    // Запись данных в файл в реальном времени
-    if (write(fd, &temperature, sizeof(float)) == -1) {
-        perror("Ошибка при записи данных в файл");
-        close(fd);
-        return MAX6675_ERROR;
-    }
-    // Закрытие файла
-    close(fd);
+    fprintf(file, "%f\n", temperature); // Записать температуру в файл
+    fclose(file); // Закрыть файл
     return MAX6675_OK;
 }
